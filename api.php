e86ba3de8ccd483e63ba0597f3e3e2e314d732ec
andriikopp <kopp93@gmail.com>
2020-08-21 09:05:39 +0300

    Update web API

--- api.php
+++ api.php
@@ -4,27 +4,31 @@ require_once("blockchain/blockchain.php");
 $_STORE_PATH = "store/";
 
 if (isset($_GET["action"])) {
-    if ($_GET["action"] == "register") {
-        $name = md5(uniqid(rand(), 1));
-        $blockchain = new BlockChain($_STORE_PATH . $name);
-        echo $name;
-    }
-
     if (isset($_GET["hash"])) {
         if ($_GET["action"] == "push" && isset($_GET["data"])) {
             $blockchain = new BlockChain($_STORE_PATH . $_GET["hash"]);
             echo $blockchain->pushData($_GET["data"]);
-        }
-
-        if ($_GET["action"] == "last") {
+        } else if ($_GET["action"] == "last") {
             $blockchain = new BlockChain($_STORE_PATH . $_GET["hash"]);
             echo $blockchain->getLastBlockData();
-        }
-
-        if ($_GET["action"] == "all") {
+        } else if ($_GET["action"] == "all") {
             $blockchain = new BlockChain($_STORE_PATH . $_GET["hash"]);
             echo $blockchain->getAllBlocksData();
+        } else {
+            echo json_encode([
+                "status" => "error",
+                "message" => "Invalid action"
+            ]);
         }
+    } else if ($_GET["action"] == "register") {
+        $name = md5(uniqid(rand(), 1));
+        $blockchain = new BlockChain($_STORE_PATH . $name);
+        echo $name;
+    } else {
+        echo json_encode([
+            "status" => "error",
+            "message" => "Hash identifier is missing"
+        ]);
     }
 }
 ?>
